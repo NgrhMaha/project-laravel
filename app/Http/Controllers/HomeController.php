341958dc96ba7b4d801ceedf725ceea24b25c097
NgrhMaha <gustingurahmaha@gmail.com>
2021-03-05 00:31:56 +0800

    installasi template

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $title="Data Mahasiswa";
+        $data['mahasiswa']=array(
+            'nim'=>'19050201003',
+            'nama'=>'Mahayasa'
+        );
+       return view('admin.beranda', compact('title', 'data')); 
+    }
+
+    public function dashboard(){
+        $title="Data Dashboard";
+        return view('admin.dashboard', compact('title')); 
+    }
+}
